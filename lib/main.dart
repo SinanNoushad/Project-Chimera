feacf0771fd55dfc21d79ff91a2c31acb842e3ca
Sinan Noushad <sinannaushad999@gmail.com>
2025-04-25 18:01:48 +0530

    commit v1

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,32 @@ class ChimeraApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Project Chimera',
-      theme: ThemeData.dark(),
-      home: const CanvasWidget(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.grey[200],
+      ),
+      home: const ChimeraHome(),
+    );
+  }
+}
+
+class ChimeraHome extends StatelessWidget {
+  const ChimeraHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Project Chimera'),
+      ),
+      body: const CanvasWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
